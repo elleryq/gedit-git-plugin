8cdc9a380598af87f5062424001781a28ce0f747
Garrett Regier <garrettregier@gmail.com>
2013-03-18 20:59:47 -0700

    Fix unicode warning when diffing

--- git/viewactivatable.py
+++ git/viewactivatable.py
@@ -126,7 +126,7 @@ class GitPlugin(GObject.Object, Gedit.ViewActivatable):
 
             entry = tree.get_by_path(relative_path)
             file_blob = repo.lookup(entry.get_id(), Ggit.Blob.__gtype__)
-            file_contents = file_blob.get_raw_content().decode('utf-8')
+            file_contents = file_blob.get_raw_content()
             self.file_contents_list = file_contents.splitlines(True)
 
             # Remove the last empty line added by gedit automatically
